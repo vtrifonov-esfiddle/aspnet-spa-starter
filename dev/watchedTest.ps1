0d1a5fb59d713f534e1e318a183e317eefe6254b
Vladimir Trifonov <vladotrifonov@gmail.com>
2019-09-29 21:59:27 +0100

    added ASP .NET 3.0 + react boilerplate - dev/prod build in container

---
+++ dev/watchedTest.ps1
@@ -0,0 +1,6 @@
+function WatchedTest() {
+    $SourcesPath = Resolve-Path "$PSScriptRoot\..\src-test"
+    docker run --rm -it -v "$($SourcesPath):/app" -w /app mcr.microsoft.com/dotnet/core/sdk:3.0 dotnet watch test
+}
+
+WatchedTest
---
+++ src-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace src_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
