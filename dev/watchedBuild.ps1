e0cc91f85909b2786822cba0ca7dae1e2a9a396d
Vladimir Trifonov <vladotrifonov@gmail.com>
2019-10-07 14:45:08 +0100

    added dev Build and Debug vscode configurations

--- dev/watchedBuild.ps1
+++ dev/watchedBuild.ps1
@@ -2,13 +2,15 @@ function WatchedRun($hostPort = 45000) {
     $SourcesPath = Resolve-Path "$PSScriptRoot\..\src"
     docker run --rm -v "$($SourcesPath):/app" vtrifonovdocker/aspnet-react-container:dev dotnet restore
     docker run --rm -it -p "$($hostPort):5000" -v "$($SourcesPath):/app" -w /app/ClientApp vtrifonovdocker/aspnet-react-container:dev npm install
-    docker run --rm -it -p "$($hostPort):5000" -v "$($SourcesPath):/app" vtrifonovdocker/aspnet-react-container:dev dotnet watch run --urls "http://0.0.0.0:5000"
+
+    $ContainerName = "aspnet-react-container.dev"
+    docker run --rm -it -p "$($hostPort):5000" -v "$($SourcesPath):/app" --name $ContainerName vtrifonovdocker/aspnet-react-container:dev dotnet watch run --urls "http://0.0.0.0:5000"
 }
 
 function DevBuild() {
     $ContextDir = Resolve-Path "$PSScriptRoot\..\src"
-    $tag = "vtrifonovdocker/aspnet-react-container:dev"
-    docker build -f $PSScriptRoot\Dockerfile -t $tag $ContextDir
+    $Tag = "vtrifonovdocker/aspnet-react-container:dev"    
+    docker build -f $PSScriptRoot\Dockerfile -t $Tag $ContextDir
 }
 
 DevBuild
