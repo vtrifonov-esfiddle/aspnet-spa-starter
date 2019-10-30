1709d96c54b17c3feddd61d35cfbcbe267a3d192
Vladimir Trifonov <vladotrifonov@gmail.com>
2019-10-30 15:10:53 +0000

    run react unit tests in watchTest.ps1

--- dev/watchedTest.ps1
+++ dev/watchedTest.ps1
@@ -1,6 +1,11 @@
-function WatchedTest() {
+function WatchedTestDotnet() {
     $SourcesPath = Resolve-Path "$PSScriptRoot\..\src-test"
     docker run --rm -it -v "$($SourcesPath):/app" -w /app mcr.microsoft.com/dotnet/core/sdk:3.0 dotnet watch test
 }
 
-WatchedTest
+function WatchedTestReact() {
+    docker exec -it aspnet-react-container.dev npm test --prefix /app/ClientApp
+}
+
+WatchedTestReact
+# WatchedTestDotnet
