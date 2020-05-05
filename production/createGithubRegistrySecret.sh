bf732dacbe70c30a4dcd1e9e5dd357956d768042
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-06 00:04:44 +0100

    prepare kubernetes deployment scripts for deployment agent

---
+++ production/createGithubRegistrySecret.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GITHUB_USERNAME="${{ github.actor }}"
+PAT_TOKEN="${{ secrets.GITHUB_TOKEN }}"
+
+kubectl create secret docker-registry github-registry \
+  --docker-server="docker.pkg.github.com" \
+  --docker-username=$GITHUB_USERNAME \
+  --docker-password=$PAT_TOKEN
--- production/deployment-up.ps1
+++ production/deployment-up.ps1
@@ -1,2 +1,1 @@
 kubectl -f $PSScriptRoot\aspnet-react-deployment.yaml apply
-kubectl port-forward services/aspnet-react-container 8084:80
