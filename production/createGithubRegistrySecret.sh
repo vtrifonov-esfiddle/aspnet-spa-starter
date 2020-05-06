e9111a0de92508f387c5820f53f8657e2c84ca74
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-06 23:18:59 +0100

    fix read registry PAT token

--- production/createGithubRegistrySecret.sh
+++ production/createGithubRegistrySecret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GITHUB_USERNAME="${{ github.actor }}"
-# PAT_TOKEN="${{ secrets.GITHUB_TOKEN }}"
+# PAT_TOKEN="${{ secrets.GithubDockerRegistryRead }}" - Github PAT with registry read access  
 
 sudo kubectl create secret docker-registry github-registry \
   --docker-server="docker.pkg.github.com" \
