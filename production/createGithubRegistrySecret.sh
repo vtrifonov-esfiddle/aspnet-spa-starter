d9bc8b0e4c6b87d3c75dca4b0d7c438da2e13bab
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-06 23:20:45 +0100

    make sure secret gets updated if exists

--- production/createGithubRegistrySecret.sh
+++ production/createGithubRegistrySecret.sh
@@ -2,7 +2,7 @@
 # GITHUB_USERNAME="${{ github.actor }}"
 # PAT_TOKEN="${{ secrets.GithubDockerRegistryRead }}" - Github PAT with registry read access  
 
-sudo kubectl create secret docker-registry github-registry \
+sudo kubectl apply secret docker-registry github-registry \
   --docker-server="docker.pkg.github.com" \
   --docker-username=$GITHUB_USERNAME \
   --docker-password=$PAT_TOKEN
