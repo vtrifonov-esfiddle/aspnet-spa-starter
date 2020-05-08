ca4f0056847d4582df85f4df28bab96258f32259
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-08 12:56:37 +0100

    use isolated node-modeules volume to run the tests

--- .github/workflows/build/localTest/unitTest.ps1
+++ .github/workflows/build/localTest/unitTest.ps1
@@ -1,3 +1,10 @@
 $SrcDir = Resolve-Path "$PSScriptRoot\..\..\..\..\src"
 
-docker run --volume $SrcDir/ClientApp:/app/ClientApp -w /app/ClientApp node:12 bash -c "CI=true npm test"
+docker run `
+    --volume $SrcDir/ClientApp:/app/ClientApp:cached `
+    --volume react-unit-tests-node_modules:/app/ClientApp/node_modules `
+    --workdir /app/ClientApp `
+    --name react-unit-tests `
+    --rm `
+    node:12 bash `
+    -c "npm ci && CI=true npm test"    
