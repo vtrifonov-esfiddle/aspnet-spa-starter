151c48cc02b155bc2cd108aff810100931d02dbe
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-08 15:12:56 +0100

    - fix returning error code and env variable

--- .github/workflows/build/testDeployment.sh
+++ .github/workflows/build/testDeployment.sh
@@ -2,9 +2,11 @@ tryCount=0
 errorCode=1
 while [ $tryCount -le 3 ] && [ $errorCode -ne 0 ]
 do
-curl --head $ASPNET_REACT_PRODUCTION_URI
-errorCode=$?
-echo "$ASPNET_REACT_PRODUCTION_URI; try count: $tryCount; Error Code: $errorCode"
-sleep 5
-tryCount=$(( tryCount + 1 ))
-done      
+    curl --head $ASPNET_REACT_PRODUCTION_URI
+    errorCode=$?
+    echo "$ASPNET_REACT_PRODUCTION_URI; try count: $tryCount; Error Code: $errorCode"
+    sleep 5
+    tryCount=$(( tryCount + 1 ))
+done
+
+exit $errorCode
