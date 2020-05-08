5391fbc6e65ebd7f4977cfb003c9b838973418ba
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-08 11:33:28 +0100

    - make sure build waits for unit tests to run - fix build

--- .github/workflows/build/localTest/unitTest.ps1
+++ .github/workflows/build/localTest/unitTest.ps1
@@ -1,3 +1,3 @@
 $SrcDir = Resolve-Path "$PSScriptRoot\..\..\..\..\src"
 
-docker run --volume "$SrcDir/ClientApp:/app/ClientApp" -w /app/ClientApp node:12 bash -c "CI=true npm test"
+docker run --volume $SrcDir/ClientApp:/app/ClientApp -w /app/ClientApp node:12 bash -c "CI=true npm test"
