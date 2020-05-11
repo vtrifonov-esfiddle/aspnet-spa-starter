fc78d89e4166ee740aa927700635fcf8400130c5
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-11 22:45:31 +0100

    - removed deployment workflow - renamed build to ci workflow   - removed deploy-test job from ci

--- .github/workflows/deploy/createGithubRegistrySecret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# GITHUB_USERNAME="${{ github.actor }}"
-# PAT_TOKEN="${{ secrets.GithubDockerRegistryRead }}" - Github PAT with registry read access  
-
-sudo kubectl delete secret github-registry --ignore-not-found=true
-sudo kubectl create secret docker-registry github-registry \
-  --docker-server="docker.pkg.github.com" \
-  --docker-username=$GITHUB_USERNAME \
-  --docker-password=$PAT_TOKEN
