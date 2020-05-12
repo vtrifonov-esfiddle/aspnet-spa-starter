b999953ec9a07621277248e3caadd4d169de6591
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-12 12:21:54 +0100

    ability to update dev container name

--- .devcontainer/up.ps1
+++ .devcontainer/up.ps1
@@ -1,1 +1,5 @@
-docker-compose --project-name aspnet-react-container_devcontainer -f $PSScriptRoot\docker-compose.yml up -d --build
+$IsMatched = $(Get-Content .env)[1] -match "GITHUB_REPO=(.+)"
+$RepoName = $Matches[1]
+$ProjectName = "$($RepoName)_devcontainer"
+echo $ProjectName
+docker-compose --project-name $ProjectName -f $PSScriptRoot\docker-compose.yml up -d --build
