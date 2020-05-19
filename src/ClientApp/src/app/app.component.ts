a93be6ee61d3e0770f8f968bed21e65c98f76a60
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-19 21:33:08 +0000

    added weather service

--- src/ClientApp/src/app/app.component.ts
+++ src/ClientApp/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { WeatherService, IWeather } from './weather.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  constructor (weatherService: WeatherService) { 
+     weatherService.getWeather().then((weather) => {
+      this.weather = weather;
+     })
+  }
+  weather: IWeather;
   title = 'aspnet-spa-starter';
 }
---
+++ src/ClientApp/src/app/weather.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+
+export interface IWeather {
+  date: Date;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor() { }
+
+  async getWeather(weatherId: number = 1): Promise<IWeather> {
+    const response = await fetch(`/WeatherForecast/${weatherId}`);
+    const result: IWeather = await response.json();
+    result.date = new Date(result.date);
+    return result;
+  }
+}
