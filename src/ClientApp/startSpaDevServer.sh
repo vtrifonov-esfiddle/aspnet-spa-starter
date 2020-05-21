78041699f3328cc1b55313a9f5f29dd529db7b1f
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-21 13:13:28 +0100

    merged from master

--- src/ClientApp/startSpaDevServer.sh
+++ src/ClientApp/startSpaDevServer.sh
@@ -1,2 +1,8 @@
+# replace the contents of this file with the appropriate SPA Dev Server script
+#
+# angular example:
+# npm ci
+# npm run ng serve -- --host spa-dev-server
+
 npm ci
 npm run ng serve -- --host spa-dev-server
